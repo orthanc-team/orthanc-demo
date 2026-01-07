3be8bd627a5620f2a71d80561440d447b13b0127
Benoit Crickboom <pro@sleig.be>
2026-01-07 17:35:07 +0100

    prépa demo icb

---
+++ inbox-plugin.py
@@ -0,0 +1,280 @@
+# SPDX-FileCopyrightText: 2022 - 2025 Orthanc Team SRL <info@orthanc.team>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import orthanc
+import pprint
+import json
+import re
+import time
+import threading
+import typing
+##
+# This plugin adds API routes to perform custom actions:
+# - after an upload in the inbox
+# - when the custom button is clicked
+# Ideally, we would have done 2 different plugins, but Orthanc can
+# load only a single one...
+##
+
+
+
+# Orthanc "change" handlers
+def on_change(changeType, level, resource):
+
+    if changeType == orthanc.ChangeType.ORTHANC_STARTED:
+
+        # after a startup, there might be unprocessed and even untagged studies in case
+        # Orthanc has been killed during an upload -> we must delete all these studies
+
+        untagged_studies_ids = json.loads(orthanc.RestApiPost("/tools/find", json.dumps({
+        "Level": "Study",
+        "Query": {},
+        "Labels": ["processed"],
+        "LabelsConstraint": "None",
+        "Expand": False
+        })))
+
+        if len(untagged_studies_ids) > 0:
+            orthanc.LogWarning(f"INBOX: deleting {len(untagged_studies_ids)} un-processed studies at Orthanc startup")
+            orthanc.RestApiPost("/tools/bulk-delete", json.dumps({
+            "Resources": untagged_studies_ids
+            }))
+            orthanc.LogWarning(f"INBOX: deleting: done")
+        else:
+            orthanc.LogWarning(f"INBOX: no un-processed studies at Orthanc startup")
+
+    elif changeType == orthanc.ChangeType.STABLE_STUDY:
+
+        # If a study becomes stable and if it is not tagged as processed, it means that it has stayed too long in Orthanc.
+        # This is probably a study whose upload has been interrupted
+        # NOTE: define "StableStudy" to e.g 600 seconds to determine the duration 
+        study_info = json.loads(orthanc.RestApiGet(f"/studies/{resource}"))
+        if not "processed" in study_info["Labels"]:
+            orthanc.LogWarning(f"INBOX: study {resource} stabilized without being processed -> deleting it")
+            orthanc.RestApiDelete(f"/studies/{resource}")
+
+    elif changeType == orthanc.ChangeType.JOB_SUCCESS:
+
+        # if a job is successful, and if this job is a DICOM store (or DW stow), we want to delete the study
+        job_info = json.loads(orthanc.RestApiGet(f"/jobs/{resource}"))
+        study_uid = None
+        if job_info["Type"] == "DicomModalityStore":
+            study_uid = job_info["Content"]["ParentResources"][0]
+            orthanc.RestApiDelete(f"/studies/{study_uid}")
+        
+
+    elif changeType == orthanc.ChangeType.JOB_FAILURE:
+
+        # if a job fails, we don't retry because the study has already been deleted
+        # and the user has been notified
+        orthanc.LogError(f"++++++++ job {resource} has failed, it could be a good idea to try to understand the cause...")
+        
+        #orthanc.RestApiPost(f"/jobs/{resource}/resubmit", "")
+
+
+def mark_invalid_field(result, field_name, message):
+    result[field_name] = {
+       "IsValid": False,
+       "Message": message
+    }
+
+def mark_valid_field(result, field_name):
+    result[field_name] = {
+        "IsValid": True
+    }
+
+# Orthanc Rest API callback to validate the form of the inbox
+def on_post_validate_form(output, uri, **request):
+
+    if request['method'] != 'POST':
+        output.SendMethodNotAllowed('POST')
+
+    payload = json.loads(request['body'])
+    form_fields = payload["FormFields"]
+
+    result = {}
+
+    ## PatientEmail
+    sender_name = form_fields['SenderName']
+    if not sender_name:
+        mark_invalid_field(result, 'SenderName', "")
+    else:
+        mark_valid_field(result, 'SenderName')
+
+
+    output.AnswerBuffer(json.dumps(result), 'application/json')
+
+# keep track of the currently running jobs and their source studies
+store_lock = threading.Lock()
+commit_jobs = {}
+current_commit_id = 0
+
+
+class StudyProcess:
+    anonymization_progress: int = 0
+    anonymization_completed: bool = False
+
+    def __init__(self, uploaded_study_id: str):
+        self.uploaded_study_id = uploaded_study_id
+    
+    def set_anonymization_complete(self):
+        self.anonymization_completed = True
+    
+    def set_anonymization_progress(self, pct: int):
+        self.anonymization_progress = pct
+
+    def get_pct_complete(self):
+        if self.anonymization_completed:
+            return 100
+        else:
+            return self.anonymization_progress
+
+
+class CommitJob:
+    study_processes: typing.Dict[str, StudyProcess]
+
+    current_message: str
+    has_failed: bool = False
+    is_complete: bool = False
+
+    def __init__(self, uploaded_studies_ids: typing.List[str], form_fields: dict, current_message: str):
+        self.current_message = current_message
+
+        self.study_processes = {}
+        for study_id in uploaded_studies_ids:
+            self.study_processes[study_id] = StudyProcess(uploaded_study_id=study_id)
+
+    def get_pct_complete(self):
+        total_progress = 0
+        
+        for (study_id, study_process) in self.study_processes.items():
+            total_progress += study_process.get_pct_complete()
+        return total_progress / len(self.study_processes)
+
+
+# Orthanc Rest API callback called after every upload in the inbox
+def on_post_inbox_commit(output, uri, **request):
+    global current_commit_id
+    global store_lock
+    global commit_jobs
+
+    if request['method'] != 'POST':
+        output.SendMethodNotAllowed('POST')
+    
+    payload = json.loads(request['body'])
+    uploaded_studies_ids = payload["OrthancStudiesIds"]
+    form_fields = payload["FormFields"]
+    sender_name = form_fields['SenderName']
+
+    orthanc.LogInfo(f"INBOX: committing upload for {sender_name}")
+
+
+    if len(uploaded_studies_ids) > 1:
+        message = f"Wait while {len(uploaded_studies_ids)} studies are being processed"
+    else:
+        message = f"Wait while the study is being processed"
+
+    with store_lock:
+        current_commit_id += 1
+        commit_jobs[current_commit_id] = CommitJob(uploaded_studies_ids=uploaded_studies_ids,
+                                                   form_fields=form_fields,
+                                                   current_message=message)
+        
+        response = {
+            "CommitId": current_commit_id,
+            "Message": message
+        } 
+
+    # Already send the response although we will only start processing the data.
+    # The monitoring callback will report the status that is shared through the commit_jobs data
+    output.AnswerBuffer(json.dumps(response), 'application/json')
+
+    # for study_id in uploaded_studies_ids:
+    #     anonymized_study_id = None
+    #     orthanc.LogInfo(f"INBOX: processing {study_id}")
+
+    #     study_info = json.loads(orthanc.RestApiGet(f"/studies/{study_id}"))
+
+    #     # modify the study
+    #     job = json.loads(orthanc.RestApiPost(f"/studies/{study_id}/modify", json.dumps({
+    #         "Replace": {
+    #             "AccessionNumber": patient_email
+    #         },
+    #         "Force": True,
+    #         "Synchronous": False
+    #     })))
+
+    #     job_id = job['ID']
+    #     orthanc.LogInfo(f"INBOX: Created processing job {job_id}")
+
+    #     # monitor the anonymization job and report progress in the commit_jobs data
+    #     has_failed = False
+    #     job_completed = False
+    #     while not job_completed and not has_failed:
+    #         time.sleep(1)
+    #         job = json.loads(orthanc.RestApiGet(f"/jobs/{job_id}"))
+    #         if job['State'] == 'Success':
+    #             job_completed = True
+    #             with store_lock:
+    #                 commit_jobs[current_commit_id].study_processes[study_id].anonymization_completed = True
+    #                 commit_jobs[current_commit_id].study_processes[study_id].anonymization_progress = 100
+    #                 anonymized_study_id = job['Content']['ID']
+    #                 orthanc.LogInfo(f"INBOX: Processing job {job_id} completed: {anonymized_study_id}")
+    #         elif job['State'] == 'Failure':
+    #             has_failed = True
+    #             with store_lock:
+    #                 commit_jobs[current_commit_id].has_failed = True
+    #                 commit_jobs[current_commit_id].current_message = f"One of the study could not be processed.  Ref job: {job_id}"
+    #         else:
+    #             with store_lock:
+    #                 commit_jobs[current_commit_id].study_processes[study_id].anonymization_progress = job['Progress']
+
+    #     # label the anonymized study
+    #     if anonymized_study_id:
+    #         orthanc.RestApiPut(f"/studies/{anonymized_study_id}/labels/processed", "")
+
+
+    #     # delete the original study
+    #     orthanc.RestApiDelete(f"/studies/{study_id}")
+
+    with store_lock:
+        commit_jobs[current_commit_id].is_complete = True
+        commit_jobs[current_commit_id].current_message = "All studies have been uploaded and processed."
+
+
+# Orthanc Rest API callback called to monitor processing after a commit
+def on_post_monitor_processing(output, uri, **request):
+    global store_lock
+    global commit_jobs
+
+    if request['method'] != 'POST':
+        output.SendMethodNotAllowed('POST')
+
+    # the payload is the response from the commit route
+    payload = json.loads(request['body'])
+
+    commit_id = payload["CommitId"]
+    response = {}
+
+    with store_lock:
+        if commit_id in commit_jobs:
+            commit_job = commit_jobs[commit_id]
+
+            pprint.pprint(commit_job)
+
+            response = {
+                "PctProcessed": commit_job.get_pct_complete(),
+                "HasFailed": commit_job.has_failed,
+                "IsComplete": commit_job.is_complete,
+                "Message": commit_job.current_message
+            }
+
+    output.AnswerBuffer(json.dumps(response), 'application/json')
+
+
+orthanc.RegisterRestCallback('/plugins/inbox/validate-form', on_post_validate_form)
+orthanc.RegisterRestCallback('/plugins/inbox/commit', on_post_inbox_commit)
+orthanc.RegisterRestCallback('/plugins/inbox/monitor-processing', on_post_monitor_processing)
+
+orthanc.RegisterOnChangeCallback(on_change)
